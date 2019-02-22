bba73f0bc366697f52eaa1146dd63b689bc53d43
shona-nabwire <smnabwire@gmail.com>
2019-02-22 11:48:02 +0300

    sorting function

--- list_sort.py
+++ list_sort.py
@@ -1,15 +1,29 @@
 from collections import defaultdict
 
 def list_sort():
+               
     listtobesorted = ([2, 0, 6, 5, 1, 7, 'z', 'a'])
-    d = defaultdict(list)
-    for item in listtobesorted:
-        if isinstance(item, int):
-            if (item % 2 == 0):
-                d['evens'].append(item)
-            elif (item % 2 > 0):
-                d['odds'].append(item)
-        else:
-            d['chars'].append(item)
-    print(d)
+    expected_types = {int, str}
+    evens = []
+    odds = []
+    chars = []
+    d = dict()
+    try:
+        if all(any(isinstance(item, t) for t in expected_types) for item in listtobesorted):
+            for item in listtobesorted:
+                if isinstance(item, int):
+                    if (item % 2 == 0):
+                        evens.append(item)
+                    elif (item % 2 > 0):
+                        odds.append(item)
+                else:
+                    chars.append(item)
+            d['evens'] = sorted(evens)
+            d['odds'] = sorted(odds)
+            d['chars'] = sorted(chars)
+            print(d)
+        else: 
+            raise ValueError
+    except ValueError:
+        print("invalid input") 
 list_sort()
